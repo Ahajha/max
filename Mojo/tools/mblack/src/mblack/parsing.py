bd1b2578ecd39e2385fe8dd01aeaef709726b2df
Alex Trotta <ahajha@gmail.com>
2026-09-06 22:48:35 -0400

    simplify lib2to3parse

--- Mojo/tools/mblack/src/mblack/parsing.py
+++ Mojo/tools/mblack/src/mblack/parsing.py
@@ -79,50 +79,46 @@ def lib2to3_parse(src_txt: str) -> Node:
     if not src_txt.endswith("\n"):
         src_txt += "\n"
 
-    grammars = [pygram.mojo_grammar]
+    grammar = pygram.mojo_grammar
     errors = {}
-    for grammar in grammars:
-        drv = driver.Driver(grammar)
+    drv = driver.Driver(grammar)
+    try:
+        result = drv.parse_string(src_txt, True)
+        if isinstance(result, Leaf):
+            result = Node(syms.file_input, [result])
+        return result
+
+    except ParseError as pe:
+        lineno, column = pe.context[1]
+        lines = src_txt.splitlines()
         try:
-            result = drv.parse_string(src_txt, True)
-            break
+            faulty_line = lines[lineno - 1]
+        except IndexError:
+            faulty_line = "<line number missing in source>"
+        hint = _parse_error_hint(pe, lines, lineno)
+        msg = f"Cannot parse: {lineno}:{column}: {faulty_line}"
+        if hint:
+            msg += f"\n{hint}"
+        errors[grammar.version] = InvalidInput(msg)
 
-        except ParseError as pe:
-            lineno, column = pe.context[1]
-            lines = src_txt.splitlines()
-            try:
-                faulty_line = lines[lineno - 1]
-            except IndexError:
-                faulty_line = "<line number missing in source>"
-            hint = _parse_error_hint(pe, lines, lineno)
-            msg = f"Cannot parse: {lineno}:{column}: {faulty_line}"
-            if hint:
-                msg += f"\n{hint}"
-            errors[grammar.version] = InvalidInput(msg)
+    except TokenError as te:
+        # In edge cases these are raised; and typically don't have a "faulty_line".
+        lineno, column = te.args[1]
+        errors[grammar.version] = InvalidInput(
+            f"Cannot parse: {lineno}:{column}: {te.args[0]}"
+        )
 
-        except TokenError as te:
-            # In edge cases these are raised; and typically don't have a "faulty_line".
-            lineno, column = te.args[1]
-            errors[grammar.version] = InvalidInput(
-                f"Cannot parse: {lineno}:{column}: {te.args[0]}"
-            )
+    # Choose the latest version when raising the actual parsing error.
+    assert len(errors) >= 1
+    exc = errors[max(errors)]
 
-    else:
-        # Choose the latest version when raising the actual parsing error.
-        assert len(errors) >= 1
-        exc = errors[max(errors)]
+    if matches_grammar(src_txt, pygram.python_grammar) or matches_grammar(
+        src_txt, pygram.python_grammar_no_print_statement
+    ):
+        original_msg = exc.args[0]
+        raise InvalidInput(f"{original_msg}\n{PY2_HINT}") from None
 
-        if matches_grammar(src_txt, pygram.python_grammar) or matches_grammar(
-            src_txt, pygram.python_grammar_no_print_statement
-        ):
-            original_msg = exc.args[0]
-            raise InvalidInput(f"{original_msg}\n{PY2_HINT}") from None
-
-        raise exc from None
-
-    if isinstance(result, Leaf):
-        result = Node(syms.file_input, [result])
-    return result
+    raise exc from None
 
 
 def matches_grammar(src_txt: str, grammar: Grammar) -> bool:
